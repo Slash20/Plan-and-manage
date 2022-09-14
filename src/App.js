55f520f532b85f9b69623f69a11f1370442eb8fe
K1ngVlad <kingvlad3008@gmail.com>
2022-09-14 18:54:02 +0400

    изменение структуры

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import React from 'react';
 import Home from './pages/Home/Home';
 import Footer from './rezability-components/Footer/Footer';
 import Header from './rezability-components/Header/Header';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <main className="main">
-        <Home />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={Home} />
+            <Route path="/blog" />
+            <Route path="/features" />
+            <Route path="/pricing" />
+            <Route path="/documentation" />
+          </Routes>
+        </BrowserRouter>
       </main>
       <Footer />
     </div>
