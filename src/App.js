001cd5a8112904d5976ffa6fa2268f096ed5eb7f
K1ngVlad <kingvlad3008@gmail.com>
2022-09-14 18:07:28 +0400

    Создание файловой структуры

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import Home from './pages/Home/Home';
+import Footer from './rezability-components/Footer/Footer';
+import Header from './rezability-components/Header/Header';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <main className="main">
+        <Home />
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
