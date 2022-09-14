f3bf41441156de281cf33ff06038dc750a2a6c25
ZEKasd <80667229506@ukr.net>
2022-09-14 20:42:06 +0300

    Второй_набросок_хедера

--- src/rezability-components/Header/Header.jsx
+++ src/rezability-components/Header/Header.jsx
@@ -3,12 +3,12 @@ import './Header.css';
 
 const Header = function ()  {
   return (    
-  <div class="header">
-    <div class="header-left">
-      <div class="header-logo">
+  <div className="header">
+    <div className="header-left">
+      <div className="header-logo">
         <img src="#" width="80" height="80" alt="logo"/>
       </div>
-      <ul class="main-nav">
+      <ul className="main-nav">
         <li><a href="#">Home</a></li>
         <li><a href="#">Blog</a></li>
         <li><a href="#">Features</a></li>
@@ -16,16 +16,16 @@ const Header = function ()  {
         <li><a href="#">Documentation</a></li>
       </ul>
     </div>
-    <div class="header-right">
-      <div class="social">
-        <ul class="social-link">
-          <li><a href="#" class="social-link-Facebook"><img src="" alt="Facebook"></img></a></li>
-          <li><a href="#" class="social-link-Telegram"><img src="" alt="Telegram"></img></a></li>
-          <li><a href="#" class="social-link-Twitter"><img src="" alt="Twitter"></img></a></li>
-          <li><a href="#" class="social-link-M"><img src="" alt="M"></img></a></li>
+    <div className="header-right">
+      <div className="social">
+        <ul className="social-link">
+          <li><a href="#" className="social-link-Facebook"><img src="" alt="Facebook"></img></a></li>
+          <li><a href="#" className="social-link-Telegram"><img src="" alt="Telegram"></img></a></li>
+          <li><a href="#" className="social-link-Twitter"><img src="" alt="Twitter"></img></a></li>
+          <li><a href="#" className="social-link-M"><img src="" alt="M"></img></a></li>
         </ul>
       </div>
-      <div class="start-btn">
+      <div className="start-btn">
         <button value="Get started"/>
       </div>
     </div>
