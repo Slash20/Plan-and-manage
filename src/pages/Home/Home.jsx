0da7406bee229393ef357aca75bcfa3ac12c479d
Slash20 <kurenkov200234@inbox.ru>
2022-09-16 14:52:28 +0300

    Убрал лишний код

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import './Home.css';
 
+// Код ниже скорее всего лишний, как и футер 
 const Home = () => {
-  return <div className="Home">Home</div>;
+  return <div className="Home"></div>;
 };
 
 export default Home;
