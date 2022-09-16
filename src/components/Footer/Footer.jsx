755a0c342d69c7b4cd95ed192c7108b7e5aad9c9
Slash20 <kurenkov200234@inbox.ru>
2022-09-16 14:52:28 +0300

    Убрал лишний код

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import './Footer.css';
 
+// Этот код тоже скорее всего лишний
 const Footer = () => {
-  return <footer className="Footer">Footer</footer>;
+  return <footer className="Footer"></footer>;
 };
 
 export default Footer;
