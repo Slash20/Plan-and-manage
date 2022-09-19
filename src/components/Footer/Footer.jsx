c3c3960b9d3b65d08c4cb82c2ae14c89292eac19
Slash20 <kurenkov200234@inbox.ru>
2022-09-19 21:16:08 +0300

    Исправление структуры проекта

--- src/components/Footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './Footer.css';
-
-// Этот код тоже скорее всего лишний
-const Footer = () => {
-  return <footer className="Footer"></footer>;
-};
-
-export default Footer;
