3595afeb57ed648e6a2b6a72f5c35634ad8ed787
ZEKasd <80667229506@ukr.net>
2022-09-28 22:26:36 +0300

    start_features

---
+++ src/pages/Home/Features/Features.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./Features.css"
+import Clouds from "../../../img/svg/features-clouds.svg"
+
+const Features = () => {
+  return (
+    <div className='features'>
+        <div className='read-about'>
+            <div className='content'>
+                <div className='title'>
+
+                </div>
+                <div className='features-content'>
+
+                </div>
+                <div className='buttons'>
+
+                </div>
+            </div>
+            <img src={Clouds}/>
+        </div>
+    </div>
+  )
+}
+
+export default Features
