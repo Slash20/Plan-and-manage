396fba105447214d5732641d0da9e009562787ab
K1ngVlad <kingvlad3008@gmail.com>
2022-11-13 15:08:38 +0400

    dadada

---
+++ src/pages/Home/Blog/Arrow/Arrow.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Arrow.css';
+
+const Arrow = () => {
+  return <div className="dasdsadasd">Arrow</div>;
+};
+
+export default Arrow;
--- src/pages/Home/Blog/Blog.jsx
+++ src/pages/Home/Blog/Blog.jsx
@@ -1,58 +1,73 @@
-import React from 'react'
-import './Blog.css'
+import React from 'react';
+import './Blog.css';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import {Pagination, Navigation} from 'swiper';
+import { Pagination, Navigation } from 'swiper';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import 'swiper/css';
-import Img1 from '../../../img/svg/Blog img1.svg'
-import Img2 from '../../../img/svg/Blog img2.svg'
-import Img3 from '../../../img/svg/Blog img3.svg'
+import Img1 from '../../../img/svg/Blog img1.svg';
+import Img2 from '../../../img/svg/Blog img2.svg';
+import Img3 from '../../../img/svg/Blog img3.svg';
+import Arrow from './Arrow/Arrow';
+import { useRef } from 'react';
 
 function Blog() {
+  const arrowLeft = useRef(null);
+  const arrowRight = useRef(null);
   return (
-    <div className='blog'>
-        <div className='blog-title'>
-            <div className='blog-title-our'>OUR RESOURCES</div>
-            <div className='blog-title-start'>Start reading our blog</div>
+    <div className="blog">
+      <div className="blog-title">
+        <div className="blog-title-our">OUR RESOURCES</div>
+        <div className="blog-title-start">Start reading our blog</div>
+      </div>
+      <div className="blog-swiper">
+        <div className="dasda" ref={arrowLeft}>
+          {'<'}
         </div>
-        <div className='blog-swiper'>
-            <Swiper
-                modules={[Navigation, Pagination]}
-                spaceBetween={0}
-                slidesPerView={1}
-                navigation
-                pagination={{ clickable: true }}
-                onSwiper={(swiper) => console.log(swiper)}
-                onSlideChange={() => console.log('slide change')}
-                >
-                    <SwiperSlide className='blog-slide'>
-                            <div className='blog-ilustration'>
-                                <img className='blog-img1' src={Img1} alt='img'/>
-                                <img className='blog-img2' src={Img2} alt='img'/>
-                                <img className='blog-img3' src={Img3} alt='img'/>
-                            </div>
-                            <div className='blog-text'>
-                                <div className='blog-text-title'>How to start planning</div>
-                                <div className='blog-text-text'>
-                                    Quidam vocibus eum ne, erat consectetuer voluptatibus ut nam. Eu usu vidit tractatos, vero tractatos ius an, in mel diceret persecuti. Natum petentium principes mei ea. Tota everti periculis vis ei, quas tibique pro at, eos ut decore ...
-                                </div>
-                                <div className='blog-text-btns'>
-                                    <button className='blog-btn1'>Read now</button>
-                                    <button className='blog-btn2'>Add to your bookmarks</button>
-                                </div>
-                            </div>
-                    </SwiperSlide>
-                <SwiperSlide>
-                    
-                </SwiperSlide>
-                <SwiperSlide>
-                    
-                </SwiperSlide>
-            </Swiper>
+        <Swiper
+          modules={[Navigation, Pagination]}
+          spaceBetween={0}
+          slidesPerView={1}
+          //   navigation
+          navigation={{
+            nextEl: '.dasdo',
+            prevEl: '.dasda',
+            enabled: true,
+          }}
+          pagination={{ clickable: true }}
+          onSwiper={(swiper) => console.log(swiper)}
+          onSlideChange={() => console.log('slide change')}
+        >
+          <SwiperSlide className="blog-slide">
+            <div className="blog-ilustration">
+              <img className="blog-img1" src={Img1} alt="img" />
+              <img className="blog-img2" src={Img2} alt="img" />
+              <img className="blog-img3" src={Img3} alt="img" />
+            </div>
+            <div className="blog-text">
+              <div className="blog-text-title">How to start planning</div>
+              <div className="blog-text-text">
+                Quidam vocibus eum ne, erat consectetuer voluptatibus ut nam. Eu
+                usu vidit tractatos, vero tractatos ius an, in mel diceret
+                persecuti. Natum petentium principes mei ea. Tota everti
+                periculis vis ei, quas tibique pro at, eos ut decore ...
+              </div>
+              <div className="blog-text-btns">
+                <button className="blog-btn1">Read now</button>
+                <button className="blog-btn2">Add to your bookmarks</button>
+              </div>
+            </div>
+          </SwiperSlide>
+
+          <SwiperSlide></SwiperSlide>
+          <SwiperSlide></SwiperSlide>
+        </Swiper>
+        <div className="dasdo" ref={arrowRight}>
+          {'>'}
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Blog
+export default Blog;
